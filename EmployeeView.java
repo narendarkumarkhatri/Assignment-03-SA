d047d17ee5334c3f8d4155a146aa63822533fb6c
Narendar Kumar <80641218+narendarkumarkhatri@users.noreply.github.com>
2021-05-22 10:48:22 +0500

    Create  EmployeeView.java

---
+++ EmployeeView.java
@@ -0,0 +1,11 @@
+public class EmployeeView
+{
+    public void EmployeeDetails(int employeeID, String firstName, String lastName, String gender, String hiredDate, Float salary) {
+        System.out.println("Employee");
+        System.out.println("Employee Id: " + employeeID);
+        System.out.println("Employee Name: " + firstName + " " + lastName);
+        System.out.println("Gender: " + gender);
+        System.out.println("Hired Date: " + hiredDate);
+        System.out.println("Salary: " + salary);
+    }
+}
